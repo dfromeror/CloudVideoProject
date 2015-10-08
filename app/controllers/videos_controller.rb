bc9fff917ac64a98f27242f01fc97baf5631c0b6
Rios, Sergio [JANCO] <srios6@ITS.JNJ.com>
2015-10-07 21:26:36 -0500

    Ajuste de convertidor para un solo video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,14 +3,17 @@ class VideosController < ApplicationController
   include ControllerVideoProcessor
 
   def convert_videos
-    videos = Video.where(video_status_id: 1)
-
-    videos.each do |video|
-      begin
-        convert_to_mp4 video
-      rescue Exception => msg
-        logger.fatal msg
-      end
+    #videos = Video.where(video_status_id: 1)
+    video = Video.where(:video_status_id => 1).order(created_at: :desc)[0]
+    if video
+      convert_to_mp4 video
     end
+    # videos.each do |video|
+    #   begin
+    #     convert_to_mp4 video
+    #   rescue Exception => msg
+    #     logger.fatal msg
+    #   end
+    # end
   end
 end
