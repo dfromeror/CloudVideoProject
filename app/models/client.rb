4307b629e4071dd8cc919751b9b5e699417d3277
Rios, Sergio [JANCO] <srios6@ITS.JNJ.com>
2015-10-20 08:48:01 -0500

    Modelos User y Client con DynamoDB

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,16 +1,13 @@
 class Client
 	#has_many :users
+  dynamo_db = AWS::DynamoDB.new
+  @@table = dynamo_db.tables[self.name.pluralize.downcase].load_schema
 
 	def self.all
-		db = AWS::DynamoDB.new
-		table = db.tables['clients']
-		table.hash_key = {:id => :string}
-		@clients = table.items.select
+		@clients = @@table.items.select
 	end
 
 	def self.find(id)
-		db = AWS::DynamoDB.new
-    table = db.tables['clients'].load_schema
     @client = table.items.where(id: id)
 	end
 
